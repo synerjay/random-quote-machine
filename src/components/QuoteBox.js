4be816147678305e8734e622266a73e7b5fa2d5d
Jerome Tolentino <tolent.jay@gmail.com>
2021-05-15 16:08:37 +0900

    Added quotes fetch promise API

--- src/components/QuoteBox.js
+++ src/components/QuoteBox.js
@@ -1,6 +1,27 @@
-import { Fragment } from 'react';
+import { Fragment, useEffect } from 'react';
+
+const quotesURL =
+  'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json';
 
 const QuoteBox = () => {
+  // useState hook
+
+  // Use Effect Hook to fetch API at first render
+  useEffect(() => {
+    fetchRandomQuote();
+  }, []);
+
+  // Fetch randomQuote RestAPI
+
+  const fetchRandomQuote = () => {
+    fetch(quotesURL)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => console.log(err));
+  };
+
   return (
     <Fragment>
       <div className='QuoteBox'>
